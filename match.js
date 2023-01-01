1b94bb2f574dc84fe5f163492178a8c99fa6d5ae
Karsten Yin <42128680+karsteny@users.noreply.github.com>
2023-01-01 04:27:54 +0000

    annoyed that i can't proceed

--- match.js
+++ match.js
@@ -6,7 +6,6 @@ import {createDivWithClassAndText, gimmeADivider} from '/helpfulHTML.js';
 // }
 
 export class twoTeamMatch {
-  
   title;
   status;
   redTeamA;
@@ -15,7 +14,8 @@ export class twoTeamMatch {
   blueTeamA;
   blueTeamB;
   blueScore;
-  element;
+  elementA;
+  elementB;
   constructor(title, status, redTeamA, redTeamB, redScore, blueTeamA, blueTeamB, blueScore){
     this.title = title;
     this.status = status;
@@ -25,10 +25,21 @@ export class twoTeamMatch {
     this.blueTeamA = blueTeamA;
     this.blueTeamB = blueTeamB;
     this.blueScore = blueScore;
+    this.elementA = this.createHTMLElement();
+    this.elementB = this.createHTMLElement();
   }
   setScore(redScore, blueScore){
     this.redScore = redScore;
     this.blueScore = blueScore;
+  }
+  setStatus(status){
+    this.status = status;
+  }
+  getElementA(){
+    return this.elementA;
+  }
+  getElementB(){
+    return this.elementB;
   }
   //literally makes an entire 2 elements for the scrolling list
   createHTMLElement(){
@@ -53,10 +64,17 @@ export class twoTeamMatch {
     matchStat.appendChild(gimmeADivider());
     matchStat.appendChild(createDivWithClassAndText("dark-ftc-red team", this.redTeamB));
     matchStat.appendChild(gimmeADivider());
-    matchStat.appendChild(createDivWithClassAndText("very-light-gray score", ""));
-    matchStat.appendChild(gimmeADivider());
-    matchStat.appendChild(createDivWithClassAndText("very-light-gray score", ""));
-    matchStat.appendChild(gimmeADivider());
+    if(this.redScore == null){
+      matchStat.appendChild(createDivWithClassAndText("very-light-gray score", ""));
+      matchStat.appendChild(gimmeADivider());
+      matchStat.appendChild(createDivWithClassAndText("very-light-gray score", ""));
+      matchStat.appendChild(gimmeADivider());
+    }else{
+      matchStat.appendChild(createDivWithClassAndText("light-ftc-red score", this.redScore));
+      matchStat.appendChild(gimmeADivider());
+      matchStat.appendChild(createDivWithClassAndText("light-ftc-blue score", this.blueScore));
+      matchStat.appendChild(gimmeADivider());
+    }
     matchStat.appendChild(createDivWithClassAndText("dark-ftc-blue rank", "-"));
     matchStat.appendChild(gimmeADivider());
     matchStat.appendChild(createDivWithClassAndText("dark-ftc-blue team", this.blueTeamA));
--- script.js
+++ script.js
@@ -4,6 +4,20 @@ import {twoTeamMatch} from '/match.js';
 var list = document.getElementById("match_list");
 var scrollA = document.getElementById("scroll-container-a");
 var scrollB = document.getElementById("scroll-container-b");
+
+function updateList(){
+  let request = new XMLHttpRequest();
+  request.open("GET", "https://ftc-api.firstinspires.org/v2.0/2022/schedule/USCASDSDGAM2?teamNumber=7159&end=999");
+  request.setRequestHeader("Authorization", "Basic " + btoa("karsteny:NO"));
+
+  request.send();
+  request.onload = () => {
+    console.log(request.response);
+    if(request.status == 200){
+      console.log(JSON.parse(request.response));
+    }
+  }
+}
 //calculates distance, speed, etc for the animation, or turns it off if it all fits
 function updateScroll(){
   if(scrollA.offsetHeight > list.offsetHeight){
@@ -11,6 +25,7 @@ function updateScroll(){
       scrollA.getAnimations()[0].cancel();
       scrollB.getAnimations()[0].cancel();
     }
+    //SCROLL SPEED HERE (offsetHeight*a number)
     scrollA.animate({top: ["0em", -scrollA.offsetHeight+"px"]}, {duration: scrollA.offsetHeight*27, easing: "linear", iterations: Infinity});
     scrollB.animate({top: ["0em", -scrollA.offsetHeight+"px"]}, {duration: scrollA.offsetHeight*27, easing: "linear", iterations: Infinity});
   }else{
@@ -22,13 +37,14 @@ function updateScroll(){
   }
 }
 //test code!
-var test = new twoTeamMatch("Qualification 69", "In Progress", 7159, 7159, null, 6969, 6969, null);
-scrollA.appendChild(test.createHTMLElement());
-scrollB.appendChild(test.createHTMLElement());
-var test2 = new twoTeamMatch("Qualification 21", "Never", 4, 5, 32, 6, 7, 33);
-scrollA.appendChild(test2.createHTMLElement());
-scrollB.appendChild(test2.createHTMLElement());
-var rando = document.createElement("div");
-rando.style.height = "2em";
-scrollA.appendChild(rando);
-updateScroll();
+// var test = new twoTeamMatch("Qualification 69", "In Progress", 7159, 7159, null, 6969, 6969, null);
+// scrollA.appendChild(test.getElementA());
+// scrollB.appendChild(test.getElementB());
+// var test2 = new twoTeamMatch("Qualification 21", "Never", 4, 5, 32, 6, 7, 33);
+// scrollA.appendChild(test2.getElementA());
+// scrollB.appendChild(test2.getElementB());
+// var rando = document.createElement("div");
+// rando.style.height = "2em";
+// scrollA.appendChild(rando);
+// updateScroll();
+updateList();
