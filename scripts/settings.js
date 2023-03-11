fb6ccfa52e2bd1eee45155ba7f82c29173881e2f
karsteny <42128680+karsteny@users.noreply.github.com>
2023-03-10 16:55:17 -0800

    settings save, now we need to have it be used

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,7 +1,26 @@
 function save(){
-
+    chrome.storage.sync.set({
+        username: document.getElementById("username").value,
+        token: document.getElementById("token").value,
+        refresh: document.getElementById("refresh").value
+      }, function(){
+        document.getElementById("status").textContent = "Saved!";
+        setTimeout(function(){
+            document.getElementById("status").textContent = "";
+        }, 1000)
+      });
 }
 function loadOptions(){
-    document.getElementById("username").value="eeeeeee";
+    document.getElementById("username").value="eeeeeeeeee";
+    chrome.storage.sync.get({
+        refresh: 30,
+        username: "",
+        token: ""
+      }, function(items) {
+        document.getElementById("username").value = items.username;
+        document.getElementById("token").value = items.token;
+        document.getElementById("refresh").value = items.refresh;
+      });
 }
 document.addEventListener('DOMContentLoaded', loadOptions);
+document.getElementById("submit").addEventListener("click", save);
